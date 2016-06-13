977513565bd326e38b817b79adade44fe802b632
Kirill Smelkov <kirr@nexedi.com>
2016-06-13 20:25:43 +0300

    pycapicompat.h: Switch to MIT License
    There is no valid Public Domain in Europe (the only valid Public Domain
    in continental law countries is after 75 years after author's death).
    
    So switch to MIT license, which is one of the most permissive OSI
    approved ones.
    
    Thanks @jp for clarifying and reminding about this.

--- pycapicompat.h
+++ pycapicompat.h
@@ -2,7 +2,28 @@
 #define _PYCAPICOMPAT_H_
 
 /* pycapicompat.h - Extra CAPI functions for compatibility between various C-based Python implementations
- * This file has been placed into Public Domain.
+ * Copyright (C) 2016 Nexedi SA and Contributors.
+ * This file is licensed under the MIT license:
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * ~~~~~~~~
  *
  * The recommended way to use pycapicompat.h is to copy it from canonical
  * location to your project on as-needed basis and include locally.
